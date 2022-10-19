31fe496a3e9fdbc8b7589dad19b6a3c0d75ee496
Allyson Mitchem <allysonmitchem@gmail.com>
2022-10-19 10:10:14 -0600

    add navlink for login, still trying to link properly

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import { NavLink } from 'react-router-dom'
 import './Navbar.css'
 import { logInUser } from '../api-adapter'
 
@@ -6,17 +7,20 @@ import { logInUser } from '../api-adapter'
 const Navbar = () =>{
     const [login, setLogIn]= useState([])
     useEffect(()=>{
-        setLogIn()
-    })
+        setLogIn(logInUser)
+    }, [])
     return (
         <header>
             <h1 >Stranger's Things</h1>
             <form
             className= "user"
-            onSubmit={handleSubmit}>
+            //</header>onSubmit={handleSubmit}
+            >
+
             <button className="Home">Home</button>
             <button className="Posts">Posts</button>
-            <button className="Login" >Login</button> 
+            <NavLink className= "login" to={logInUser} >Login</NavLink>
+            {/* <button className="Login" >Login</button/users/login>  */}
             {/*Nav bar option buttons*/}
 
 
