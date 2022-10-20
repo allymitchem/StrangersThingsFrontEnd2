c4a12083fe2f44bc6db7f845e2cac34a8aeef471
Leonardo <cruzleonardo831@gmail.com>
2022-10-20 11:12:12 -0500

    logout

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import { redirect, NavLink } from 'react-router-dom'
 
+const logOut =()=>{
+    localStorage.removeItem('token')
+    return 
+}
+
 
 export default Logout;
