57693c74d8c69e7f81f9e0b2affba5b41d19efc6
Leonardo <cruzleonardo831@gmail.com>
2022-10-20 11:10:55 -0500

    buttons working

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,32 @@
 import React, {useState} from 'react'
-import { redirect } from 'react-router-dom'
+import { redirect, NavLink } from 'react-router-dom'
 import Register from './Register'
+import { logInUser } from '../api-adapter'
  
-
-const Login = () => {
-    const [currentUser, setCurrentUser] = useState("")
-
-    async function handleLogin (event){
-        event.preventDefault ()
-
+const Login = (props)=> {
+    // const [password, setPassword]= useState("")
+    async function handleSubmit(event){
+    // RegisterUser()
+        event.preventDefault()
+        const username= event.target[0].value
+        const password = event.target[1].value
+        const registeredUser = await logInUser(username, password)
+        console.log(registeredUser)
+       
+        //check for message or for error.message
+        
+        const token = registeredUser.token 
+        console.log (token)
+        localStorage.removeItem('token')
+        localStorage.setItem('token',token)
     }
+
     return (
         <div>
     {/* {`Login page`} */}
     <div className= 'logInBox'> 
       {/* need to center log in boxes */}
-        <form onSubmit = {handleLogin}>
+        <form onSubmit = {handleSubmit}>
             <h1 style={{color: "red", fontSize: "50px", fontFamily:'ITC Benguiat'}}>Log In</h1>
             <label htmlFor="username">Username: </label><br></br>
             <input id='username' type='text'  required/><br></br>
@@ -23,8 +34,7 @@ const Login = () => {
             <input id="password" type= "password" /><br></br>
             <button type="submit"> Log In </button><br></br>
            
-            <button style={{textDecoration: "underline", cursor: "pointer"}}type="submit"> Don't have an account? Sign Up </button> 
-            {/*need to get rid of borders around button*/}
+            <NavLink to= "/register">Don't have an account? Sign Up</NavLink>
             </form>
             </div>
     </div>
---
+++ src/components/Logout.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { redirect, NavLink } from 'react-router-dom'
+
+
+export default Logout;
