61a733208fae860b8d9ba01c14d6ae0431bb85b2
Allyson Mitchem <allysonmitchem@gmail.com>
2022-10-21 13:27:10 -0600

    add component for my messages

---
+++ src/components/MyMessages.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const MyMessages= () => {
+    
+}
+
+
+export default MyMessages 
--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,6 @@ export {default as Register} from './Register'
 export {default as Login} from './Login'
 export {default as CreatePosts} from './CreatePosts'
 export {default as PostDetails} from './PostDetails'
+export {default as MyMessages} from './MyMessages'
 // export {default as SinglePost} from './SinglePost'
 
